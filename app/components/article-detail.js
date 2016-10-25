c84f793b81e9563e9c1fc7c578ce13a77c73ef4e
Mark Lawson and J. Ryan Conklin <j.@>
2016-10-25 14:53:21 -0700

    Add a delete feature to Article-detail component

--- app/components/article-detail.js
+++ app/components/article-detail.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(article) {
+      this.sendAction('destroyArticle', article);
+    }
+  }
 });
--- app/routes/article.js
+++ app/routes/article.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('article', params.article_id);
+  },
+  actions: {
+    destroyArticle(article) {
+      article.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
