96bdc8e8fb88195c8fb77e4afec12e68b6a71f15
Mark Lawson and J. Ryan Conklin <j.@>
2016-10-25 15:35:38 -0700

    add update feature to article

--- app/components/article-detail.js
+++ app/components/article-detail.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   actions: {
     delete(article) {
       this.sendAction('destroyArticle', article);
+    },
+    update(article, params) {
+      this.sendAction('update', article, params);
     }
   }
 });
