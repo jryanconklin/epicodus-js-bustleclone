328dab745b68eadc94cf3775f05e37acaa92c29d
Mark Lawson and J. Ryan Conklin <j.@>
2016-10-25 16:39:10 -0700

    add text preview to article tile and styling

--- app/models/article.js
+++ app/models/article.js
@@ -5,5 +5,16 @@ export default DS.Model.extend({
   author: DS.attr(),
   body: DS.attr(),
   image: DS.attr(),
-  date: DS.attr()
+  date: DS.attr(),
+
+  preview: Ember.computed('body', function() {
+    var body = `${this.get('body')}`;
+    var words = body.split(" ");
+    if (words.length > 15) {
+      var previewDisplay = words.slice(0,15).join(" ") + '...';
+    } else {
+      var previewDisplay = words.join(" ");
+    }
+    return previewDisplay;
+  })
 });
